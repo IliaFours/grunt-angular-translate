a604cf27c83a5e417016440cf447611d28dc05ff
IliaFours <ilia.fours@gmail.com>
2014-02-12 18:51:06 +0300

    Task
    Add deep (thought .) localization.
    
    Not save mode doesnt work. Statictic breaks.

--- tasks/angular-translate.js
+++ tasks/angular-translate.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
   grunt.registerMultiTask('i18nextract', 'Generate json language file(s) for angular-translate project', function () {
 
     // Shorcuts!
-    var _ = require('lodash');
+    var _ = grunt.util._;
     var _log = grunt.log;
     var _file = grunt.file;
 
@@ -27,23 +27,21 @@ module.exports = function (grunt) {
       dest = this.data.dest || '.',
       jsonSrc = _file.expand(this.data.jsonSrc || []),
       jsonSrcName = _.union(this.data.jsonSrcName || [], ['label']),
-      defaultLang = this.data.defaultLang || '.',
+      // defaultLang = this.data.defaultLang || '.',
       interpolation = this.data.interpolation || {startDelimiter: '{{', endDelimiter: '}}'},
       source = this.data.source || '',
-      nullEmpty = this.data.nullEmpty || false,
-      namespace = this.data.namespace || false,
       prefix = this.data.prefix || '',
-      safeMode = this.data.safeMode ? true : false,
+      // safeMode = this.data.safeMode ? true : false,
       suffix = this.data.suffix || '.json',
       results = {};
 
     var escapeRegExp = function (str) {
-      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    }
+      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    };
 
     var _extractTranslation = function (regexName, regex, content, results) {
       var r;
-      _log.debug("---------------------------------------------------------------------------------------------------");
+      _log.debug('---------------------------------------------------------------------------------------------------');
       _log.debug('Process extraction with regex : "' + regexName + '"');
       _log.debug(regex);
       regex.lastIndex = 0;
@@ -53,97 +51,65 @@ module.exports = function (grunt) {
         // Except for plural hack [STRING, KEY, ARRAY_IN_STRING]
         if (r.length >= 2) {
           var translationKey, evalString;
-          var translationDefaultValue = "";
+          var translationDefaultValue = '';
 
           switch (regexName) {
-            case 'HtmlDirectivePluralFirst':
-              var tmp = r[1];
-              r[1] = r[2];
-              r[2] = tmp;
-            case 'HtmlDirectivePluralLast':
-              evalString = eval(r[2]);
-              if (_.isArray(evalString) && evalString.length >= 2) {
-                translationDefaultValue = "{NB, plural, one{" + evalString[0] + "} other{" + evalString[1] + "}" + (evalString[2] ? ' ' + evalString[2] : '');
-              }
-              translationKey = r[1].trim();
-              break;
-            default:
-              translationKey = r[1].trim();
-          }
+          case 'HtmlDirectivePluralFirst':
+            var tmp = r[1];
+            r[1] = r[2];
+            r[2] = tmp;
+          case 'HtmlDirectivePluralLast':
+            evalString = eval(r[2]);
+            if (_.isArray(evalString) && evalString.length >= 2) {
+              translationDefaultValue = '{NB, plural, one{' + evalString[0] + '} other{' + evalString[1] + '}' + (evalString[2] ? ' ' + evalString[2] : '');
+            }
+            translationKey = _(r[1]).strip();
+            break;
+          default:
+            translationKey = _(r[1]).strip();
+        }
 
-          // Avoid empty translation
-          if (translationKey === "") {
+          // Avoid emptu translation
+          if (translationKey === '') {
             return;
           }
 
-          switch (regexName) {
-            case "HtmlFilterSimpleQuote":
-            case "JavascriptServiceSimpleQuote":
-            case "JavascriptFilterSimpleQuote":
-            case "HtmlNgBindHtml":
-              translationKey = translationKey.replace(/\\\'/g, "'");
-              break;
-            case "HtmlFilterDoubleQuote":
-            case "JavascriptServiceDoubleQuote":
-            case "JavascriptFilterDoubleQuote":
-              translationKey = translationKey.replace(/\\\"/g, '"');
-              break;
-          }
-
-          // Case sub namespace!
-          if (namespace && translationKey.indexOf('.') !== -1) {
-            // Save translation key with point
-            var fullTranslationKey = translationKey;
-            // Split translation key by point
-            var splitted = translationKey.split('.');
-            var isValidNamespace = splitted.length > 1;
-            // Check if valid namespace (avoid endpoint ad empty part)
-            for (var i in splitted) {
-              isValidNamespace = (splitted[ i ] != "");
-            }
-            // Get default value
-            var curObj, obj = translationDefaultValue;
-            if (isValidNamespace) {
-              // Remove first one
-              translationKey = splitted[0];
-              splitted.splice(0,1);
-              // Build sub namespace
-              var curObj = obj = {};
-
-              var curDefault = translationKey;
-              for (var index in splitted) {
-
-                if (splitted.length - 1 == index) {
-                  curObj[ splitted[ index ] ] = translationDefaultValue;
-                } else {
-                  if (results[translationKey] && results[translationKey][ splitted[ index ] ]) {
-                    curObj[ splitted[ index ] ] = _.extend({}, results[translationKey][ splitted[ index ] ]);
-                  } else {
-                    curObj[ splitted[ index ] ] = _.extend({}, curObj[ splitted[ index ] ]);
-                  }
-                  curObj = curObj[ splitted[ index ] ];
-                }
-              }
-              results[ translationKey ] = _.extend({}, results[translationKey], obj);
-            } else {
-              results[ translationKey ] = translationDefaultValue;
-            }
-          } else {
-            results[ translationKey ] = translationDefaultValue;
-          }
-
+          results[ translationKey ] = translationDefaultValue;
         }
       }
+    };
+
+    var parseTranslationKey = function(key) {
+      var parts = key.split('.'), result = {};
+
+      if(parts.length > 1) {
+        result[parts[0]] = parseTranslationKey(parts.slice(1).join('.'));
+      } else {
+        result[key] = key;
+      }
+
+      return result;
+    };
+
+    var makeTranslations = function(translationKeys) {
+      var result = {};
+
+      _.forEach(translationKeys, function(key) {
+        result = _.merge(result, parseTranslationKey(key));
+      });
+
+      return result;
     };
 
     // Regexs that will be executed on files
     var regexs = {
       HtmlFilterSimpleQuote: escapeRegExp(interpolation.startDelimiter) + '\\s*\'((?:\\\\.|[^\'\\\\])*)\'\\s*\\|\\s*translate(:.*?)?\\s*' + escapeRegExp(interpolation.endDelimiter),
       HtmlFilterDoubleQuote: escapeRegExp(interpolation.startDelimiter) + '\\s*"((?:\\\\.|[^"\\\\\])*)"\\s*\\|\\s*translate(:.*?)?\\s*' + escapeRegExp(interpolation.endDelimiter),
-      HtmlDirective: '<[^>]*translate[^{>]*>([^<]*)<\/[^>]*>',
+      HtmlDirective: '<[^>]*translate(?!\\s*[}"=-])[^{>]*>([^<]*)<\/[^>]*>',
+      HtmlDirectiveAttribute: 'translate="((?:\\\\.|[^"\\\\])*)"',
       HtmlDirectivePluralLast: 'translate="((?:\\\\.|[^"\\\\])*)".*angular-plural-extract="((?:\\\\.|[^"\\\\])*)"',
       HtmlDirectivePluralFirst: 'angular-plural-extract="((?:\\\\.|[^"\\\\])*)".*translate="((?:\\\\.|[^"\\\\])*)"',
-      HtmlNgBindHtml: 'ng-bind-html="\\s*\'((?:\\\\.|[^\'\\\\])*)\'\\s*\\|\\s*translate(:.*?)?\\s*"',
+      HtmlTitleDirectiveAttribute: 'translate-title="((?:\\\\.|[^"\\\\])*)"',
       JavascriptServiceSimpleQuote: '\\$translate\\(\\s*\'((?:\\\\.|[^\'\\\\])*)\'[^\\)]*\\)',
       JavascriptServiceDoubleQuote: '\\$translate\\(\\s*"((?:\\\\.|[^"\\\\])*)"[^\\)]*\\)',
       JavascriptFilterSimpleQuote: '\\$filter\\(\\s*\'translate\'\\s*\\)\\s*\\(\\s*\'((?:\\\\.|[^\'\\\\])*)\'[^\\)]*\\)',
@@ -169,8 +135,10 @@ module.exports = function (grunt) {
           case "HtmlFilterSimpleQuote":
           case "HtmlFilterDoubleQuote":
           case "HtmlDirective":
+          case "HtmlDirectiveAttribute":
           case "HtmlDirectivePluralLast":
           case "HtmlDirectivePluralFirst":
+          case "HtmlTitleDirectiveAttribute":
           case "JavascriptFilterSimpleQuote":
           case "JavascriptFilterDoubleQuote":
             // Match all occurences
@@ -218,40 +186,13 @@ module.exports = function (grunt) {
       return currentArray;
     };
 
-    /**
-     * Recurse feed translation object (utility for namespace)
-     * INPUT: {"NS1": {"NS2": {"VAL1": "", "VAL2": ""} } }
-     * OUTPUT: {"NS1": {"NS2": {"VAL1": "NS1.NS2.VAL1", "VAL2": "NS1.NS2.VAL2"} } }
-     * @param {Object} data
-     * @param {string?} path
-     * @private
-     */
-    var _recurseFeedDefaultNamespace = function (data, path) {
-      var path = path || '';
-      if (_.isObject(data)) {
-        for (var key in data) {
-          if (_.isObject(data)) {
-            data[ key ] = _recurseFeedDefaultNamespace(data[ key ], path != '' ? path + '.' + key : key);
-          }
-        }
-        return data;
-      } else {
-        if (data == null && data == "") {
-          // return default data if empty/null
-          return path;
-        } else {
-          return data;
-        }
-      }
-    };
-
     // Parse all extra files to extra
     jsonSrc.forEach(function (file) {
       _log.debug("Process extra file: " + file);
       var content = _file.readJSON(file);
       var recurseData = _recurseObject(content);
       for (var i in recurseData) {
-        results[ recurseData[i].trim() ] = '';
+        results[ _(recurseData[i]).strip() ] = '';
       }
     });
 
@@ -274,79 +215,54 @@ module.exports = function (grunt) {
 
       _log.subhead('Process ' + lang + ' : ' + filename);
 
+      translations = makeTranslations(_.keys(results));
 
       if (!_file.exists(filename)) {
         _log.debug('File doesn\'t exist');
-        translations = _.cloneDeep(results);
       } else {
         _log.debug('File exist');
         json = _file.readJSON(filename);
-        // Extend data if no namespace
-        if (!namespace) {
-          _.extend((translations = _.cloneDeep(results) ), json);
-        } else {
-          // Merge recursively objDest into objSrc
-          var _recurseExtend = function (objSrc, objDest) {
-            if (_.isObject(objDest)) {
-              Object.getOwnPropertyNames(objDest).forEach(function(index) {
-                if (_.isObject( objDest[ index ])) {
-                  objDest[index] = _recurseExtend(objSrc[index], objDest[index]);
-                } else {
-                  objDest[index] = objSrc && objSrc[index] ? objSrc[ index ] : "";
-                }
-              });
-            }
-            return _.cloneDeep(objDest);
-          };
-          translations = _recurseExtend(json, _.cloneDeep(results));
-        }
+        translations = _.merge(translations, json);
       }
 
+      // TODO: find out how many translations have been added and removed
       // Make some stats
-      for (var k in translations) {
-        var translation = translations[k];
-        var isJson = _.isString(json[k]);
-        var isResults = _.isString(results[k]);
 
-        nbTra++;
+      // for (var k in translations) {
+      //   var translation = translations[k];
+      //   var isJson = _.isString(json[k]);
+      //   var isResults = _.isString(results[k]);
 
-        // Case namespace
-        if (namespace && _.isObject(translation) && lang === defaultLang) {
-          translations[ k ] = _recurseFeedDefaultNamespace(translations[ k ]);
-        }
+      //   nbTra++;
 
-        // Case empty translation
-        if (translation === '') {
-          if (lang === defaultLang) {
-            translations[ k ] = k;
-          } else {
-            // Test if option to set empty to null
-            if (nullEmpty) {
-              translations[ k ] = null;
-            }
-            nbEmpty++;
-          }
-        }
-        // Case new translation (exist into src files but not in json file)
-        if (!isJson && isResults) {
-          nbNew++;
-        }
-        // Case deleted translation (exist in json file but not into src files)
-        if (isJson && !isResults) {
-          nbDel++;
-          if (!safeMode) {
-            delete translations[ k ];
-          }
-        }
-      };
+      //   // Case empty translation
+      //   if (translation === '') {
+      //     if (lang === defaultLang) {
+      //       translations[ k ] = k;
+      //     } else {
+      //       nbEmpty++;
+      //     }
+      //   }
+      //   // Case new translation (exist into src files but not in json file)
+      //   if (!isJson && isResults) {
+      //     nbNew++;
+      //   }
+      //   // Case deleted translation (exist in json file but not into src files)
+      //   if (isJson && !isResults) {
+      //     nbDel++;
+      //     if (!safeMode) {
+      //       delete translations[ k ];
+      //     }
+      //   }
+      // };
       // Some information for the output
       if (!_file.exists(destFilename)) {
         _log.subhead('Create file: ' + destFilename);
       }
 
-      _log.writeln('Empty: ' + nbEmpty + ' (' + Math.round(nbEmpty / nbTra * 100) + '%) / New: ' + nbNew + ' / Deleted: ' + nbDel);
+      //_log.writeln('Empty: ' + nbEmpty + ' (' + Math.round(nbEmpty / nbTra * 100) + '%) / New: ' + nbNew + ' / Deleted: ' + nbDel);
       // Write JSON file for lang
-      _file.write(destFilename, JSON.stringify(translations, null, 4));
+      _file.write(destFilename, JSON.stringify(translations, null, 2));
 
     });
 
